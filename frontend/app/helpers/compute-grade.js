525514d8997255473bb0f3e61c57de536107037e
mewben <melvinsoldia@outlook.com>
2014-10-21 16:15:01 +0800

    Fix precision number addition

--- frontend/app/helpers/compute-grade.js
+++ frontend/app/helpers/compute-grade.js
@@ -4,8 +4,9 @@ export default Em.Handlebars.makeBoundHelper(function(mt, ft) {
 	if (! Em.$.isNumeric(mt) || ! Em.$.isNumeric(ft)) {
 		return;
 	}
-	var ave = ((mt - 0) + (ft - 0)) / 2 * 10;
-	var res = Math.floor(ave) / 10;
+
+	var ave = ((mt - 0) + (ft - 0)) / 2;
+	var res = parseFloat(ave.toPrecision(12));
 
 	if (res > 3) {
 		return new Em.Handlebars.SafeString( '<span style="color: red; font-weight:bold">' + res.toFixed(1) + '</span>' );
