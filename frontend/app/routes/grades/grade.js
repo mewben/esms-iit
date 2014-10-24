de53ad9e055c3512734bb0b0cd9455b771aeecee
pipay <philliplibay@gmail.com>
2014-10-24 12:07:05 +0800

    Added isChanged flag on grades-update, php & js

--- frontend/app/routes/grades/grade.js
+++ frontend/app/routes/grades/grade.js
@@ -3,6 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
 	model: function(params) {
+		//get model from php
 		return this.controllerFor('g').getJSON('/grades/' + params.subjcode + '/' + params.section);
+	},
+
+	setupController: function(controller, model) {
+		var oData = Ember.copy(model.data, true);
+		controller.set('oData', oData);
+		controller.set('model', model);
 	}
 });
